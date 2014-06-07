950dc1d0923be1ef31aafb3e6c8378f3e9283ef2
chetanme <chetanme@usc.edu>
2014-06-07 14:16:24 -0700

    PHP group module commit

---
+++ group/group/html/people.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="ISI Group website, Karma , data integration , data mashups">
+    <meta name="author" content="ANIMESH MANGLIK">
+<link rel="icon" href="http://www.isi.edu/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="http://www.isi.edu/favicon.ico" type="image/x-icon" />
+    <title>Information Integration Research Group</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../link/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../link/css/justified-nav.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+		
+      	<div class="row">
+      	 <div class="col-lg-9 ">
+   
+  <h1>Information Integration Research Group</h1>
+      
+  <p><em >
+  <a href="http://www.isi.edu">Information Sciences Institute</a>, 
+  <a href="http://www.viterbi.usc.edu">Viterbi School of Engineering</a></em></p>
+      	 </div>
+      	 <div class="col-lg-3" id="logo">
+      	 <img class="pull-right visible-lg" src="../link/images/usc-shield-name-black.png">
+      	 </div>
+      	</div>
+        <div class="masthead">
+        <ul class="nav nav-justified">
+          <li><a href="../">Overview</a></li>
+          <li  class="active"><a href="">People</a></li>
+          <li><a href="projects.html">Project</a></li>
+          <li><a href="publication.html" >Publication</a></li>
+          <li><a href="videos.html">Videos</a></li>
+          <li><a href="downloads.html">Downloads</a></li>
+        </ul>
+      </div>
+
+
+	<iframe src="http://fusion1.isi.edu:3001/" width="100%" height="2670px" marginheight="0" frameborder="0"></iframe>
+
+
+      <!-- Site footer -->
+      <div class="footer" style="margin-top:0;">
+        <p>&copy;2013 The University of Southern California</p>
+      </div>
+
+    </div> <!-- /container -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+</body>
+</html>
---
+++ group/group/html/php_script/get_info.php
@@ -0,0 +1,5 @@
+ 
+ <link type="text/css" rel="Stylesheet" href="publication.css" />
+ <?php $contents = file_get_contents("http://www.bibbase.org/show?bib=isi.edu/integration/group_docs/group.bib&groupby=year&css=1&simplegroups=1&proxy=1"); 
+		  print_r($contents);
+	   ?>
